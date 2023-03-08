2a5978f934c230e710d5ca3b6109d9b3c141eba3
Lelik <kentaskis.l@gmail.com>
2023-03-08 19:05:22 +0100

    Test for account controller

--- src/test/java/com/project/bankproj/controller/AccountControllerTest.java
+++ src/test/java/com/project/bankproj/controller/AccountControllerTest.java
@@ -5,51 +5,65 @@ import com.project.bankproj.service.interfaces.AccountServiceImpl;
 import com.project.bankproj.util.DtoCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@AutoConfigureMockMvc
+@WebMvcTest(AccountController.class)
 @DisplayName("Test class for AccountController")
-@ExtendWith(MockitoExtension.class)
 class AccountControllerTest {
-    @Mock
+    @MockBean
     private AccountServiceImpl accountService;
 
-    @InjectMocks
-    private AccountController accountController;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void listAccounts() {
+    void listAccounts() throws Exception {
         final List<AccountDto> accountDtoList = new ArrayList<>();
         final AccountDto account = DtoCreator.getAccountDto();
         accountDtoList.add(account);
         when(accountService.getList()).thenReturn(accountDtoList);
 
-        final List<AccountDto> actualList = accountController.listAccounts();
+        mockMvc.perform(MockMvcRequestBuilders.get("/account"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(account.getId())))
+                .andExpect(jsonPath("$[0].name", is(account.getName())))
+                .andExpect(jsonPath("$[0].status", is(account.getStatus().toString())))
+                .andExpect(jsonPath("$[0].currency", is(account.getCurrency().toString())))
+                .andExpect(jsonPath("$[0].balance").value(account.getBalance().toString()))
+        ;
 
-        assertEquals(accountDtoList, actualList);
-        verify(accountService).getList();
     }
 
     @Test
-    void get() {
+    void get() throws Exception {
         final AccountDto account = DtoCreator.getAccountDto();
         when(accountService.getById(account.getId())).thenReturn(account);
 
-        final AccountDto actual = accountController.get(account.getId());
-
-        assertNotNull(actual);
-        assertEquals(account, actual);
-        verify(accountService).getById(account.getId());
+        mockMvc.perform(MockMvcRequestBuilders.get("/account/" + account.getId()))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(account.getId()))
+                .andExpect(jsonPath("$.name").value(account.getName()))
+                .andExpect(jsonPath("$.status", is(account.getStatus().toString())))
+                .andExpect(jsonPath("$.currency", is(account.getCurrency().toString())))
+                .andExpect(jsonPath("$.balance").value(account.getBalance()));
     }
 }
